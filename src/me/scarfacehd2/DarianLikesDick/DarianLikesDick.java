6f75b1142e63212919255bdcb3f5cf7c827cf596
scarfacehd2 <scarfacehd2@clashnia.com>
2012-12-03 15:26:13 -0500

    First commit.

---
+++ src/me/scarfacehd2/DarianLikesDick/DarianLikesDick.java
@@ -0,0 +1,20 @@
+package me.scarfacehd2.DarianLikesDick;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DarianLikesDick extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+		//TODO
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		//TODO
+	}
+	
+}
