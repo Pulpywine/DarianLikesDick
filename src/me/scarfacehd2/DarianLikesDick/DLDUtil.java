7d60cc755d453b78dc0a4c0de89758696a928988
scarfacehd2 <scarfacehd2@clashnia.com>
2012-12-04 21:29:21 -0500

    Fixed console logging.

--- src/me/scarfacehd2/DarianLikesDick/DLDUtil.java
+++ src/me/scarfacehd2/DarianLikesDick/DLDUtil.java
@@ -1,17 +1,19 @@
 package me.scarfacehd2.DarianLikesDick;
 
+import java.util.logging.Logger;
+
 public class DLDUtil {
 
-	static DarianLikesDick plugin;
+	static Logger log = Logger.getLogger("Minecraft");
 	
 	/*
 	 *  consoleMSG : Send (String)msg to the console with level (String)level.
 	 */
 	public static void consoleMSG(String level, String msg)
 	{
-		if(level == "warning") plugin.getLogger().warning("[DarianLikesDick] " + msg);
-		if(level == "severe") plugin.getLogger().severe("[DarianLikesDick] " + msg);
-		else plugin.getLogger().info("[DarianLikesDick] " + msg);
+		if(level == "warning") log.warning("[DarianLikesDick] " + msg);
+		if(level == "severe") log.severe("[DarianLikesDick] " + msg);
+		else log.info("[DarianLikesDick] " + msg);
 	}
 	
 }
